6ea2bac2fc625bd3a647cc78e2e9b108eaf1163e
Grant <grant_mixon1@baylor.edu>
2024-02-05 14:14:02 -0600

    worked through section 5

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -4,29 +4,41 @@ phonebook = {'Chris':'555−1111',
              'Katie':'555−2222',
              'Joanne':'555−3333'}
 
-
+'''
 
 print()
 print('*****  start section 1 - print dictionary ********')
 print()
+print(phonebook)
+print(len(phonebook))
 
+mydictionary = {}       #this will create an empty dictionary, but beasue of function below, we dont have to do that
 
+mydictionary = dict(m=8,n=9)     # m and n are the keys and 8,9 are the values
 
-
+print(mydictionary)
 
 print()
 print('*****  end section 1 ********')
 print()
 
 
-'''
+
 
 
 print()
 print('*****  start section 2 - search dictionary ********')
 print()
 
+name = 'Chris'
 
+if name in phonebook:
+    print(f"Name: {name} Phone Number: {phonebook[name]}")
+else:
+    print(f"{name} is not in the phone book")
+
+#phone = phonebook['Chris']     #keys are sensitive, upperase and lower case makes a difference
+#print(phone)
 
 
 
@@ -46,7 +58,12 @@ print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
+print(phonebook)
 
+phonebook['Joe'] = '555-0123'
+phonebook['Chris'] = '555-4444'
+
+print(phonebook)
 
 
 
@@ -62,8 +79,11 @@ print()
 print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
+print(phonebook)
 
+del phonebook['Chris']
 
+print(phonebook)
 
 
 print()
@@ -71,7 +91,7 @@ print('*****  end section 4 ********')
 print()
 
 
-
+'''
 
 
 
@@ -79,7 +99,11 @@ print()
 print('*****  start section 5 - iterate through keys, values, items ********')
 print()
 
+for key in phonebook:
+    print(f"The key is: {key} and the value is {phonebook[key]}") 
 
+for value in phonebook.values():    #.values() allows you to iterate through all of the values in the dict
+    print(value)
 
 
 
@@ -89,7 +113,7 @@ print('*****  end section 5 ********')
 print()
 
 
-
+'''
 
 
 print()
